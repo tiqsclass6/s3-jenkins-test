0de9c141f158f31908f60514e72bc28bfa14489e
tiqsclass6 <daquietstorm22@gmail.com>
2026-03-24 20:19:49 -0700

    TIQS fourth commit

--- README.md
+++ README.md
@@ -1,18 +1,48 @@
-# New Jenkins server test with terraform deployment and triggers
+# Jenkins Build Using Webhook and Run Terraform Script
+
+## Prerequisites
+
+- Jenkins running on EC2
+- Jenkins reachable from the internet (e.g. `http://<EC2-PUBLIC-IP>:8080`)
+- Jenkins Git plugin installed
+- Jenkins AWS Credentials plugin installed
+- Repo with Jenkinsfile and terraform script
+- Github and git plugins
+
+---
 
 ## Jenkinsfile
 
-A simple declarative Jenkinsfile
-- Clones git repo 
-- Binds AWS IAM user creds in terraform stages with AWS Creds plugin
-- Stages for terraform init and apply 
-- Destroy stage using user input 
+A simple declarative [**Jenkinsfile**](Jenkinsfile):
 
-## Terraform script 
-- A simple AWS S3 bucket is deployed
-- State file is stored in S3 backend 
-- S3 bucket name uniqueness is guranteed 
+- Clones git repo
+- Binds AWS IAM user creds in terraform stages with AWS Creds plugin
+- Stages for terraform init and apply
+- Destroy stage using user input
+
+---
+
+## Terraform File
+
+- A simple [**AWS S3 bucket**](test-bucket.tf) is deployed
+- S3 bucket name uniqueness is guaranteed
+
+---
 
 ## User data
-EC2 startup script to bootstrap Jenkins server
 
+[**EC2 startup script**](user-data.sh) to bootstrap Jenkins server
+
+---
+
+## Artifacts
+
+- **Suge WAF Output**
+  - ![suge-waf.jpg](/images/suge-waf.jpg)
+- **Jenkins Pipeline Output**
+  -![jenkins-pipeline.jpg](/images/jenkins-pipeline.jpg)
+- **Jenkins Webhook Output**
+  - ![jenkins-webhook.jpg](/images/jenkins-webhook.jpg)
+- **S3 Bucket Output**
+  - ![s3-bucket.jpg](/images/s3-bucket.jpg)
+- **T.I.Q.S. Armageddon Repo:** [**Full README.md**](https://github.com/tiqsclass6/aws-armageddon-class-7.0/blob/main/README.md)
--- test-bucket.tf
+++ test-bucket.tf
@@ -7,23 +7,50 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "class-7-state-files"                 # Name of the S3 bucket
-    key     = "class-labs/s3-jenkins-test.tfstate"          # The name of the state file in the bucket
-    region  = "us-east-1"                            # Use a variable for the region
-    encrypt = true                                   # Enable server-side encryption (optional but recommended)
-  } 
+    bucket  = "class-7-state-files"                # Name of the S3 bucket
+    key     = "class-labs/s3-jenkins-test.tfstate" # The name of the state file in the bucket
+    region  = "us-east-1"                          # Use a variable for the region
+    encrypt = true                                 # Enable server-side encryption (optional but recommended)
+  }
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "frontend" {
-  bucket_prefix = "jenkins-bucket-"
+resource "aws_s3_bucket" "tiqs-jenkins-bucket" {
+  bucket        = "tiqs-jenkins-webhook"
   force_destroy = true
-  
+
 
   tags = {
-    Name = "Jenkins Bucket"
+    Name = "T.I.Q.S. Jenkins Webhook Bucket"
   }
+}
+
+resource "aws_s3_object" "pipeline" {
+  bucket       = aws_s3_bucket.tiqs-jenkins-bucket.id
+  key          = "jenkins-pipeline.jpg"
+  source       = "${path.module}/images/jenkins-pipeline.jpg"
+  content_type = "image/jpeg"
+
+  etag = filemd5("${path.module}/images/jenkins-pipeline.jpg")
+}
+
+resource "aws_s3_object" "webhook" {
+  bucket       = aws_s3_bucket.tiqs-jenkins-bucket.id
+  key          = "jenkins-webhook.jpg"
+  source       = "${path.module}/images/jenkins-webhook.jpg"
+  content_type = "image/jpeg"
+
+  etag = filemd5("${path.module}/images/jenkins-webhook.jpg")
+}
+
+resource "aws_s3_object" "suge-waf" {
+  bucket       = aws_s3_bucket.tiqs-jenkins-bucket.id
+  key          = "suge-waf.jpg"
+  source       = "${path.module}/images/suge-waf.jpg"
+  content_type = "image/jpeg"
+
+  etag = filemd5("${path.module}/images/suge-waf.jpg")
 }
